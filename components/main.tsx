2991696770ef8d986f432f5e8b4734b9cda50161
ifan0507 <ipan.lmj0507@gmail.com>
2025-07-31 19:58:25 +0700

    revisi

--- components/main.tsx
+++ components/main.tsx
@@ -4,7 +4,7 @@ import { notFound } from "next/navigation";
 
 const Main = async () => {
   const rooms = await getRooms();
-  if (!rooms) return notFound;
+  if (!rooms) return notFound();
   return (
     <div className="max-w-screen-xl py-6 pb-20 px-4 mx-auto">
       <div className="grid gap-7 md:grid-cols-3">
